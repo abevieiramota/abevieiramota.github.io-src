8d8a986a5fc082e8b677434a8c6b725fd274612d
Abelardo Vieira Mota <abevieiramota@gmail.com>
2018-02-24 08:04:06 -0300

    source do blog

---
+++ content/a-hora-da-estrela-pt1.md
@@ -0,0 +1,9 @@
+Title: A hora da estrela - parte 1
+Date: 2018-02-24 10:00
+Category: Literatura
+Tags: Clarice Lispector
+Slug: hora-da-estrela-1
+Authors: Abelardo Vieira Mota
+Summary: Interpretações sobre A hora da estrela
+
+Teste.
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Abelardo Vieira Mota'
+SITENAME = 'Minhas ideias'
+SITEURL = 'http://abevieiramota.com.br'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Fortaleza'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/abevieiramota'),
+          ('twitter', 'https://twitter.com/abevieiramota'),)
+
+GITHUB_URL = 'http://github.com/abevieiramota'
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = '..\\crowsfoot'
+
+SIDEBAR_DIGEST = 'Analista de sistemas e clarices'
+#FAVICON = 'url-to-favicon'
+DISPLAY_PAGES_ON_MENU = True
+TWITTER_USERNAME = 'abevieiramota'
+MENUITEMS = (('Blog', SITEURL),)
+
+PROFILE_IMAGE_URL = "images/profile.jpg"
+EMAIL_ADDRESS = "abevieiramota@gmail.com"
+GITHUB_ADDRESS = "https://github.com/abevieiramota"
+TWITTER_ADDRESS = "https://twitter.com/abevieiramota"
